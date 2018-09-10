0f4ec51bb8e112ded9f536ac321a79bdca2ca2ab
Carson Rey <c.rey494@gmail.com>
2018-09-10 23:29:10 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -10,12 +10,18 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
+  equals = num1 / num2
+  equals
 end
 
 def multiplication(num1, num2)
+  equals = num1 * num2
+  equals
 end
 
 def modulo(num1, num2)
+  equals = num1 % num2
+  equals
 end
 
 def square_root(num)
